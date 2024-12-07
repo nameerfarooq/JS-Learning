363b8517d39349cc011b0927c45e50341d06bac3
nameerfarooq <nameerfarooquit@gmail.com>
2024-12-07 15:06:39 +0500

    update 'class 1/index.html'

---
+++ class 1/app.js
@@ -0,0 +1,42 @@
+// console.log("hello world")
+
+// var myName;
+// myName = "Nameer's book";
+// myName = "Abdullah"
+// var welcomeMessage = "Welcome sir, "
+// alert(welcomeMessage + myName);
+
+// var number1 = "50";
+// var number2 = 100;
+// var number3 = 20;
+// alert(number1+number2+number3)
+
+// var myNum = 100
+// var yourNum = myNum + 400  //500
+// alert(yourNum-200)
+
+// var firstNumber = 10
+// var secondNumber = 2
+// alert(firstNumber + secondNumber)
+// alert(firstNumber - secondNumber)
+// alert(firstNumber * secondNumber)
+// alert(firstNumber / secondNumber)
+// alert(firstNumber % secondNumber) it gives you remainder
+
+// alert((firstNumber + (secondNumber * 20)) - (10 / secondNumber))
+
+// var num = 10
+// alert(num+1)
+// alert(num++) //post increment
+// alert(--num)  //pre increment
+
+//prompts
+
+var userAnswer = prompt("Enter your age"); // "18"
+
+if (userAnswer != 18) {
+  alert("You are eligible for new CNIC");
+}
+else{
+    alert("Sorry you are not eligible")
+}
