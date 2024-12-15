b58579144679462f2e6db90efccb15eb48b3e640
nameerfarooq <nameerfarooquit@gmail.com>
2024-12-15 14:08:46 +0500

    class 3

---
+++ class 4/app.js
@@ -0,0 +1,32 @@
+// var cleanCities = ["makkah", "madina", "karachi", "lahore"];
+// var userCity = "peshawar";
+// var cityFound = false;
+
+// for (var i = 0; i <= 4; i++) {
+//   if (cleanCities[i] === userCity) {
+//     cityFound = true;
+//     break;
+//   }
+// }
+
+// if (cityFound == false) {
+//   console.log("City not found");
+// } else {
+//   console.log("Congrats");
+// }
+
+// var firstNames = ["abu bakar", "umer", "usman", "ali"];
+// var lastNames = ["abdullah", "ahmed", "daniyal", "owais"];
+
+
+
+// for (var i = 0; i < 4; i++) {
+
+//   for (var j = 0; j < 4; j++) {
+//     console.log(firstNames[i] + " " + lastNames[j]);
+//   }
+
+//   console.log("FIRST LOOP Iteration complete");
+
+// }
+
