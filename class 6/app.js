0881350041d13349d5837ac0cd16f8f257eb24f0
nameerfarooq <nameerfarooquit@gmail.com>
2024-12-22 15:01:44 +0500

    commit

---
+++ class 6/app.js
@@ -0,0 +1,74 @@
+// function greetuser(userName) {
+//   console.log("hello, ",userName);
+// }
+
+// greetuser("Muhammad Nameer")
+// greetuser("Zain")
+// greetuser("Atta")
+
+// function doSum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// doSum(5, 10);
+// doSum(3,3);
+// doSum(10,10);
+
+// template literals
+
+// function printTable(myNum = 5) {
+//   for (var i = 1; i <= 10; i++) {
+//     console.log(`${myNum} x ${i} = ${myNum * i}`);
+//   }
+//   return "funtion complete";
+//   console.log("FUNCTION NOT REACHED");
+// }
+
+// var funtionOutput = printTable(11);
+// console.log(funtionOutput);
+
+// var abc = 10; //20
+
+// function printABC() {
+//    abc = 20;
+//   console.log(abc);
+// }
+
+// printABC();
+
+// console.log(abc)
+
+// var myNum = 4;
+
+// switch (myNum) {
+//   case 2:
+//     console.log("two");
+//     break;
+//   case 4:
+//     console.log("four");
+//     break;
+//   case 6:
+//     console.log("six");
+//     break;
+//   default:
+//     console.log("Not in my memory");
+// }
+
+// while loop
+
+// var i = 0;
+// var valueIsCorrect = true
+// while (valueIsCorrect) {
+//  var userInput = prompt("Enter your input")
+//  if(userInput === "1"){
+//     valueIsCorrect = false
+//  }
+// }
+
+function showPopup() {
+  alert("Hi, User");
+}
+
+function showParagraphPopup() {
+  alert("THIS IS A PARAGRAPH");
+}
